39627a564b6d1a2ab28264b935deb1b927d5be53
Glitch (mountain-log) <none>
2019-03-23 19:09:48 +0000

    🌌🌺 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,55 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+app.get("/api/timestamp/:datestr?",function(req,res){
+  var d;
+  var r=req.params.datestr;
+  if(r)
+  { 
+    d=new Date(r);
+    if(d.getTime()){
+      res.json({"unix":d.getTime(),"utc":d.toUTCString()});}
+    else{
+      var re=new RegExp("^[0-9]+$");
+      if(re.test(r)){
+      d.setTime(parseInt(r,10));}
+      if(d.getTime()){
+        res.json({"unix":d.getTime(),"utc":d.toUTCString()});}
+      else{
+        res.json({"error":"Invalid Date"});
+      }
+    }
+  }
+  else{
+    d=new Date();
+    res.json({"unix":d.getTime(),"utc":d.toUTCString()});  
+  }  
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
